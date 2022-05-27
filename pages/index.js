e07021b74ee35638c4895ee2ef7a164ede736437
Keaton Krieger <keatonkrieger@Keatons-MacBook-Pro.local>
2022-05-27 11:24:25 -0700

    Basic Next layout with header

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Card } from "semantic-ui-react";
+import { Card, Button } from "semantic-ui-react";
 import factory from "../ethereum/factory";
+import Layout from "../components/Layout";
 
 class CampaignIndex extends React.Component {
   static async getInitialProps() {
@@ -23,14 +24,23 @@ class CampaignIndex extends React.Component {
 
   render() {
     return (
-      <div>
-        <link
-          async
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
-        />
-        {this.renderCampaigns()}
-      </div>
+      <Layout>
+        <div>
+          <link
+            async
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+          />
+          <h3>Open Campaigns</h3>
+          {this.renderCampaigns()}
+          <Button
+            floated="right"
+            content="Create Campaign"
+            icon="add circle"
+            primary={true}
+          />
+        </div>
+      </Layout>
     );
   }
 }
