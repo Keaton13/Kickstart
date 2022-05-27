a2561fd34b4612eaf06fd1b8237c8977043969de
Keaton Krieger <keatonkrieger@Keatons-MacBook-Pro.local>
2022-05-27 13:36:31 -0700

    Creating campaign from

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,31 +1,41 @@
 import React from "react";
-import { Form, Button, Input, FormTextArea } from "semantic-ui-react";
+import { Form, Button, Input, Message, FormTextArea } from "semantic-ui-react";
 import Layout from "../../components/Layout";
-import factory from '../../ethereum/factory';
-import web3 from '../../ethereum/web3';
+import factory from "../../ethereum/factory";
+import web3 from "../../ethereum/web3";
 
 class CampaignNew extends React.Component {
   state = {
     minimumContribution: "",
+    errorMessage: '',
+    loading: false
   };
 
   onSubmit = async (event) => {
     event.preventDefault();
 
-    const accounts = await web3.eth.getAccounts();
-    await factory.methods
-    .createCampaign(this.state.minimumContribution)
-    .send({
-        from: accounts[0]
-    })
-  };
+    this.setState({ loading: true, errorMessage: '' })
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await factory.methods
+        .createCampaign(this.state.minimumContribution)
+        .send({
+          from: accounts[0],
+        });
+    } catch (err) {
+        this.setState({ errorMessage: err.message });
+    }
+
+    this.setState({ loading: false })
+  };    
 
   render() {
     return (
       <Layout>
         <h3>Create a Campaign</h3>
 
-        <Form onSubmit={this.onSubmit}>
+        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
           <Form.Field>
             <label>Minimum Contribution</label>
             <Input
@@ -37,8 +47,8 @@ class CampaignNew extends React.Component {
               }
             />
           </Form.Field>
-
-          <Button primary>Create!</Button>
+          <Message error header="Oops!" content={this.state.errorMessage} />
+          <Button loading={this.state.loading} primary>Create!</Button>
         </Form>
       </Layout>
     );
