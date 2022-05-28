ca4f1ded22088c52d14fcc401e99cbcd704c6b30
Keaton Krieger <keatonkrieger@Keatons-MacBook-Pro.local>
2022-05-28 13:59:15 -0700

    Created Campaign

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,9 +1,9 @@
-import web3 from './web3';
-import CampaignFactory from './build/CampaignFactory.json';
+import web3 from "./web3";
+import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
-    JSON.parse(CampaignFactory.interface),
-    '0xC39cb0983767E2d82abcBE9b19dFe3C92Dc3B221'
+  JSON.parse(CampaignFactory.interface),
+  "0x4080bEDeED7B7538Dc626Be450B875cC46A37df1"
 );
 
 export default instance;
